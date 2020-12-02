f2d8161273482ad680384b1072f92d8b240fc38f
ashishiiit01 <ashishiiit01@gmail.com>
2020-12-02 12:55:59 +0530

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,149 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import {
+  Box,
+  Grid,
+  SimpleGrid,
+  GridItem,
+  WrapItem,
+  Wrap,
+  Avatar,
+  Center,
+} from "@chakra-ui/react";
+import Link from "next/link";
+export default function Home({
+  posts,
+}) {
+  return (
+    <ChakraProvider>
+      <div>
+        <Head>
+          <title>
+            Create
+            Next App
+          </title>
+          <link
+            rel="icon"
+            href="/favicon.ico"
+          />
+        </Head>
+        {/* HEADERS */}
+        <Box
+          bg="tomato"
+          w="100%"
+          p={4}
+          mb={10}
+          color="white"
+        >
+          BLOGS
+        </Box>
+        {/* POSTS  */}
+        <Box px="20">
+          <SimpleGrid
+            columns={[
+              2,
+              null,
+              3,
+            ]}
+            spacingX="30px"
+            spacingY="30px"
+          >
+            {posts.map(
+              post => (
+                <Link
+                  href={`/post/${post.id}`}
+                  key={
+                    post.name
+                  }
+                >
+                  <Box
+                    w="100%"
+                    h="60"
+                    px="2"
+                    py="5"
+                    bg="tomato"
+                    key={
+                      post.name
+                    }
+                    className={
+                      styles.box
+                    }
+                  >
+                    <Grid
+                      h="200px"
+                      templateRows="repeat(2, 1fr)"
+                      templateColumns="repeat(2, 1fr)"
+                      gap={
+                        4
+                      }
+                    >
+                      <GridItem
+                        rowSpan={
+                          1
+                        }
+                        colSpan={
+                          2
+                        }
+                        bg="tomato"
+                        color="white"
+                      >
+                        <Center>
+                          <Wrap p="2">
+                            <WrapItem>
+                              <Avatar
+                                name="Dan Abrahmov"
+                                src="https://bit.ly/dan-abramov"
+                              />
+                            </WrapItem>
+                          </Wrap>
+                          {
+                            post.name
+                          }
+                        </Center>
+                      </GridItem>
+                      <GridItem
+                        colSpan={
+                          2
+                        }
+                        bg="papayawhip"
+                        px="1"
+                        py="2"
+                      >
+                        <Center>
+                          UserName:{" "}
+                          {
+                            post.username
+                          }
+                        </Center>
+                        <Center>
+                          Email:{" "}
+                          {
+                            post.email
+                          }
+                        </Center>
+                      </GridItem>
+                    </Grid>
+                  </Box>
+                </Link>
+              )
+            )}
+          </SimpleGrid>
+        </Box>
+      </div>
+    </ChakraProvider>
+  );
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch(
+    "https://jsonplaceholder.typicode.com/users"
+  );
+  const posts = await res.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
---
+++ src/pages/post/[id].js
@@ -0,0 +1,99 @@
+import { Box, Grid, Center, Wrap, WrapItem, Avatar } from "@chakra-ui/react"
+import styles from './post.module.css'
+const getPost = async (id) => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+    const post = await res.json();
+    return post;
+};
+
+const Post = ({ post }) => {
+    return (
+        <Box bg="tomato" height="100vh" className={styles.container} >
+
+            <Box colSpan={1} px={40} py={40} className={styles.box} color="white" fontSize={18}>
+                <Center mb={40}>
+                    <Wrap >
+                        <WrapItem>
+                            <Avatar
+                                name="Dan Abrahmov"
+                                src="https://bit.ly/dan-abramov"
+                                borderRadius="105px"
+                                width={150}
+                                height={150}
+
+                            />
+                        </WrapItem>
+                    </Wrap>
+                </Center>
+                <Grid templateColumns="repeat(2, 1fr)" gap={6}>
+                    <Box >
+                        Name :
+                    </Box >
+                    <Box >
+                        {post.name}
+                    </Box >
+                </Grid>
+
+                <Grid templateColumns="repeat(2, 1fr)" gap={6}>
+                    <Box >
+                        UserName :
+                    </Box >
+                    <Box >
+                        {post.username}
+                    </Box >
+                </Grid>
+                <Grid templateColumns="repeat(2, 1fr)" gap={6}>
+                    <Box >
+                        Email :
+                    </Box >
+                    <Box >
+                        {post.email}
+                    </Box >
+                </Grid>
+
+                <Grid templateColumns="repeat(2, 1fr)" gap={6}>
+                    <Box >
+                        Company :
+                    </Box >
+                    <Box >
+                        {post.company.name}
+                    </Box >
+                </Grid>
+
+                <Grid templateColumns="repeat(2, 1fr)" gap={6}>
+                    <Box >
+                        Phone :
+                    </Box >
+                    <Box >
+                        {post.phone}
+                    </Box >
+                </Grid>
+            </Box>
+        </Box >
+    )
+}
+
+export default Post
+
+export const getStaticPaths = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const posts = await res.json();
+
+    const paths = posts.map((post) => ({
+        params: { id: post.id.toString() },
+    }));
+
+    return {
+        paths,
+        fallback: false,
+    };
+};
+
+export const getStaticProps = async ({ params }) => {
+    const post = await getPost(params.id);
+    return {
+        props: {
+            post,
+        },
+    };
+};
